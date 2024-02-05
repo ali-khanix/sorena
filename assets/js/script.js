950533aa8e14bc8748b27f9b03cb105879a76986
ali_khanix <khaniali568@gmail.com>
2024-02-05 18:59:40 +0330

    catalogue button

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function setNewIcon() {
+  document.getElementById("icon1").src =
+    "assets/images/icons/catalogue button/icon2.svg";
+}
+
+function setOldIcon() {
+  document.getElementById("icon1").src =
+    "assets/images/icons/catalogue button/icon1.svg";
+}
