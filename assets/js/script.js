e6fde266df6e145f26fb70756c825a71fa152a79
ali_khanix <khaniali568@gmail.com>
2024-02-08 19:57:56 +0330

    search section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/*=============== CHANGE ICON ===============*/
 function setNewIcon() {
   document.getElementById("icon1").src =
     "assets/images/icons/catalogue button/icon2.svg";
@@ -7,3 +8,42 @@ function setOldIcon() {
   document.getElementById("icon1").src =
     "assets/images/icons/catalogue button/icon1.svg";
 }
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+// Menu Show
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// Menu Hidden
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((link) => link.addEventListener("click", linkAction));
+
+document.addEventListener("click", (e) => {
+  if (!navMenu.contains(e.target) && !navToggle.contains(e.target)) {
+    navMenu.classList.remove("show-menu");
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    navMenu.classList.remove("show-menu");
+  }
+});
